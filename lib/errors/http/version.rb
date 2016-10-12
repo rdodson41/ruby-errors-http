946d037f68151ff9b3f9831e931fcb1866518cd6
Richard E. Dodson <richard.elias.dodson@gmail.com>
2016-10-12 15:01:40 -0400

    Bump version to 0.2.0

--- lib/errors/http/version.rb
+++ lib/errors/http/version.rb
@@ -1,5 +1,5 @@
 module Errors
   class HTTP < StandardError
-    VERSION = '0.1.0'.freeze
+    VERSION = '0.2.0'.freeze
   end
 end
--- spec/errors/http_spec.rb
+++ spec/errors/http_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Errors::HTTP do
   describe '::VERSION' do
     it do
-      expect(Errors::HTTP::VERSION).to eq '0.1.0'
+      expect(Errors::HTTP::VERSION).to eq '0.2.0'
     end
   end
 
